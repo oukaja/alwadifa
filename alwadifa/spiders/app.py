35a480c60fd952b406cfda681f3ff615e019761b
OUKAJA <oukaja.mehdi@gmail.com>
2019-04-15 17:44:15 +0100

    sites.json modified

--- alwadifa/spiders/app.py
+++ alwadifa/spiders/app.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import simplejson as json
+import os
 
 
 class AppSpider(scrapy.Spider):
     name = 'app'
-    allowed_domains = ['alwadifa']
-    start_urls = ['http://alwadifa/']
+    start_urls = []
+    data = json.load(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sites.json')))
 
     def parse(self, response):
+        print(self.data)
         pass
